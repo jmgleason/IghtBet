bea6ee9ba8f6c67e6c7a97db71a110cb324d67af
Joseph Gleason <jmgleason@mail.usf.edu>
2022-06-19 13:34:10 -0400

    Using DB values instead of dummy data for the all bets screen

--- screens/bets/AllBetsScreen.js
+++ screens/bets/AllBetsScreen.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-import BetsList from '../../components/BetsList';
-import { fetchAllBets } from '../../utils/http';
+import BetsList from "../../components/BetsList";
+import { fetchAllBets } from "../../utils/http";
 
-const AllBetsScreen = props => {
+const AllBetsScreen = (props) => {
   // This is grabbing the bets directly from the dummy data, which is set in state
-  const fetchedBets = useSelector((state) => state.bets.allBets.filter(bet => bet.ownerId == 1));
+  // const fetchedBets = useSelector((state) => state.bets.allBets.filter(bet => bet.ownerId == 1));
 
-  //const [fetchedBets, setFetchedBets] = useState([]);
+  const [fetchedBets, setFetchedBets] = useState([]);
 
-  //useEffect(() => {
-  //  async function getBets() {
-  //    const bets = await fetchAllBets();
-  //    setFetchedBets(bets);
-  //  };
+  useEffect(() => {
+    async function getBets() {
+      const bets = await fetchAllBets();
+      setFetchedBets(bets);
+      console.log(bets);
+    }
 
-  //  getBets();
-  //}, []);
+    getBets();
+  }, []);
 
-  return (
-    <BetsList bets={fetchedBets} />
-  );
+  return <BetsList bets={fetchedBets} />;
 };
 
 export default AllBetsScreen;
