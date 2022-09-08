a8c89380ce657ee9316b13c83d2ed651a574f3da
Joseph Gleason <jmgleason@mail.usf.edu>
2022-09-07 23:54:34 -0400

    Adding the drawer navigator

--- navigation/bets/DrawerNavigator.js
+++ navigation/bets/DrawerNavigator.js
@@ -1,13 +1,32 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { View } from "react-native";
 
 import StackNavigator from "./StackNavigator";
 import SettingsScreen from "../../screens/user/SettingsScreen";
+import DrawerFooter from "../../screens/drawer/DrawerFooter";
 
 const Drawer = createDrawerNavigator();
 
+function CustomDrawerContent(props) {
+  return (
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+      <DrawerFooter />
+    </View>
+  );
+}
+
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
       <Drawer.Screen
         options={{ headerShown: false }}
         name="Bets"
